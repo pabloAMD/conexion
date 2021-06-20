0a6612f4f6302b487ccace4d6190d42b7a575e16
pabloAMD <pablo.malla.abc.com@gmail.com>
2021-06-19 23:41:03 -0500

    conexion

--- logicanegocio/api.js
+++ logicanegocio/api.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 var mysql = require('mysql');
-var dbconn = mysql.createConnection({
+var dbconn = mysql.createPool({
     host: '35.193.101.225',
     user: 'root',
     password: 'Ovjgg90wIG4lC6y5',
@@ -11,53 +11,35 @@ var dbconn = mysql.createConnection({
 });
 
 
-function crearConexion() {
-    dbconn.connect(function(err) {
-        if (err) {
-            console.log('Database connection error');
-        } else {
-            console.log('Database connection successful');
-
-        }
-    });
-}
-
-
-
-
-
 router.get('/', (req, res) => {
-
-    crearConexion();
+    console.log('Database connection successful');
     dbconn.query("SELECT * FROM users", function(err, result, fields) {
         if (err) throw err;
         res.json(result);
-        dbconn.end(function(err) {
-            console.log("se cerro la  conexion")
-        });
+
+
     });
-
-
-
 
 });
 
 
 ////////////////////////////////////////
 router.post('/', (req, res) => {
-    crearConexion();
-    const cedula = req.body.user_id;
-    console.log(cedula)
 
+    console.log('Database connection successful');
     dbconn.query("INSERT INTO users  VALUES ('" + req.body.user_id + "','" + req.body.user_name + "','" + req.body.user_apellido + "','" + req.body.user_direccion + "');", function(err, result, fields) {
         if (err) throw err;
         res.json({
             "se": "inserto "
         });
-        dbconn.end(function(err) {
-            console.log("se cerro la  conexion")
-        });
+
     });
+
+
+
+
+
+
 
 });
 
